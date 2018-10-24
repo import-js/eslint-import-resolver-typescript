52c01a65b9fec236d47cc4069adeab682959e64f
Brad Zacher <brad.zacher@gmail.com>
2018-10-24 09:45:01 -0700

    [TEST] deduplicated the eslint config between test cases

---
+++ tests/baseEslintConfig.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = (dirname) => ({
+    env: {
+        es6: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: [
+        'import'
+    ],
+    rules: {
+        'import/no-unresolved': 'error',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+    },
+    settings: {
+        'import/resolver': {
+            [path.resolve(`${__dirname}/../index.js`)]: {
+                directory: dirname,
+            },
+        },
+    },
+})
