f20d56ce4d2e3cac2445eaab900fc91f1765fd6b
JounQin <admin@1stg.me>
2019-09-13 11:00:56 +0800

    ci: try to fix test error on CI due to glob

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 const { js, md, mdx } = require('@1stg/eslint-config/overrides')
 
 module.exports = {
-  extends: ['@1stg'],
+  extends: ['@1stg', 'plugin:import/typescript'],
   overrides: [js, md, mdx],
   rules: {
     'node/no-unsupported-features/es-syntax': 0,
--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -3,10 +3,10 @@ const { ts } = require('@1stg/eslint-config/overrides')
 module.exports = {
   extends: '@1stg',
   overrides: [
+    ...ts,
     {
-      ...ts,
+      files: '*.ts',
       rules: {
-        ...ts.rules,
         '@typescript-eslint/no-use-before-define': [
           2,
           {
