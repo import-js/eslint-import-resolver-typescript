1ffcd834931ebc1f721543ed89d071a91fadb1ae
JounQin <admin@1stg.me>
2019-10-06 09:03:32 +0800

    fix: add pretest script which is required

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-const { js } = require('@1stg/eslint-config/overrides')
-
-module.exports = {
-  extends: ['@1stg', 'plugin:import/typescript', 'plugin:mdx/recommended'],
-  overrides: [js],
-  rules: {
-    'node/no-unsupported-features/es-syntax': 0,
-  },
-}
