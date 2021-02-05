82ef3573fa1258e0de8d8181de57ae7109e35ec5
Joakim Carlstein <joakim@beng.se>
2021-02-05 12:48:23 +0100

    feat: remove any querystring from imports (#67)

---
+++ tests/withQuerystring/index.ts
@@ -0,0 +1,7 @@
+// import svg without querystring
+import './image.svg'
+import './subfolder/image.svg'
+
+// import svg with querystring
+import './image.svg?raw'
+import './subfolder/image.svg?raw'
