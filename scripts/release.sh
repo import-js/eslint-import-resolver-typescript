01f525ecd02523ef02d127bc280d591ac26e8cfe
JounQin <admin@1stg.me>
2022-07-01 02:16:51 +0800

    ci: integration with changesets (#118)

--- scripts/release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PKG_VERSION=$(jq -r '.version' package.json)
-
-git fetch origin v"$PKG_VERSION" || {
-  yarn standard-version -a --release-as "$PKG_VERSION"
-  git push --follow-tags origin master
-  npm publish
-}
